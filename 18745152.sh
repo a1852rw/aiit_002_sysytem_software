e17e4bc3b064f99266043ec930bb1195c6f65ed9
a1852rw <a1852rw@aiit.ac.jp>
2018-06-10 13:13:41 +0900

    The First file commit now

---
+++ 18745152.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+n=$(cat hantei)
+n1=( $n - 1 )
+
+for n2 in `seq 2 $n1`
+do 
+        echo ${n} 
+        ret=`expr $n % $n2`  
+
+        echo "ret=${ret}"
+
+        if [ ${ret} -eq 0 ]
+                then 
+                        echo "入力された値は合成数です"
+                exit
+        else
+                break
+        fi
+done
+
+echo "入力された値は素数です"
+
+exit 0
