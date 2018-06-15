9b2d67873536c5b668f9582f8255febbef790336
a1852rw <a1852rw@aiit.ac.jp>
2018-06-16 00:42:56 +0900

    test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,22 @@
 !/bin/sh
 
-echo "thin is test"
+result=$(./18745152.sh)
+
+if [ $# -ne 2 ]; then 
+	echo "input 2 argments" 
+	exit 1 
+fi
+
+expr $1 + $2 > /dev/null 2>&1 
+
+if [ $? -ge 2 ]; then 
+	echo "input natural number"
+	exit 1 
+fi
+
+if [ $1 -eq $2 ]; then 
+	echo "$1 is equal to $2" 
+else
+	echo "$1 is not equal to $2" 
+fi
+
