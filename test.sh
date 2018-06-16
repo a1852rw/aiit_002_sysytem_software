69b610da5c5b42a584562207322e8d5e0cbf4a7b
a1852rw <a1852rw@aiit.ac.jp>
2018-06-16 21:16:16 +0900

    new code

--- test.sh
+++ test.sh
@@ -1,23 +1,28 @@
 !/bin/sh
 
-result=$(./18745152.sh)
-
-if [ $# -ne 2 ]; then 
-	echo "input 2 argments" 
-	exit 1 
-fi
-
-expr $1 + $2 > /dev/null 2>&1 
-
-if [ $? -ge 2 ]; then 
-	echo "input natural number"
-	exit 1 
-fi
-
-if [ $1 -eq $2 ]; then 
-	echo "$1 is equal to $2" 
+if $? = 0 ; then
+	echo "OK no problem"
 else
-	echo "$1 is not equal to $2" 
+	echo "Non good check your code"
 fi
 
-# まずはサンプルコードを参考にした
+# 以下スライドのコードを参考に構成したものの正常に動かなかった
+# result=$(./18745152.sh)
+
+# if [ $# -ne 2 ]; then 
+#	echo "input 2 argments" 
+#	exit 1 
+# fi
+#
+#expr $1 + $2 > /dev/null 2>&1 
+#
+#if [ $? -ge 2 ]; then 
+#	echo "input natural number"
+#	exit 1 
+#fi
+#
+#if [ $1 -eq $2 ]; then 
+#	echo "$1 is equal to $2" 
+#else
+#	echo "$1 is not equal to $2" 
+#fi
